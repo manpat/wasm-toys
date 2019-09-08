cb99bf5d52db7f69d59b11e9a3fc22916be1e473
Patrick Monaghan <0x5f.manpat@gmail.com>
2019-09-08 03:27:15 +0100

    Update prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,6 +2,7 @@ pub use common::*;
 pub use failure::{ensure, bail, format_err};
 
 pub use crate::DT;
-pub use crate::{EngineResult, EngineClient};
+pub use crate::{Ticks, EngineResult, EngineClient};
+pub use crate::{console_log, console_warn, console_error};
 
 pub use crate::graphics::*;
